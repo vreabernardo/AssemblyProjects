dc4a4db44c1756de2e33508958e3f69fadd3608f
vrea@quiet <vreabernardo@gmail.com>
2024-03-20 23:14:20 +0000

    key generation based on seed and length of filename

--- projeto_1/encript.c
+++ projeto_1/encript.c
@@ -1,21 +1,44 @@
 #include <stdio.h>
-#include <stdlib.h> // for atoi function
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+#define ERROR_MESSAGE_FORMAT "Encryption of %s failed.\n"
 
 void encrypt_file(const char *input_file, const char *output_file, int seed);
+void generateKey(char *key, int length);
 
 int main(int argc, char *argv[])
 {
     if (argc != 4)
     {
-        printf("Usage: %s input_file output_file seed\n", argv[0]);
+        char error_message[100];
+        snprintf(error_message, sizeof(error_message), ERROR_MESSAGE_FORMAT, argv[1]);
+        printf("%s", error_message);
         return 1;
     }
 
-    int seed = atoi(argv[3]);
-    encrypt_file(argv[1], argv[2], seed);
+    encrypt_file(argv[1], argv[2], atoi(argv[3]));
     return 0;
 }
 
 void encrypt_file(const char *input_file, const char *output_file, int seed)
 {
+    srand(seed);
+
+    int input_length = strlen(input_file);
+    char key[input_length + 1];
+
+    generateKey(key, input_length);
+
+    // printf("Key: %s\n", key);
+}
+
+void generateKey(char *key, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        key[i] = rand();
+    }
+    key[length] = '\0';
 }

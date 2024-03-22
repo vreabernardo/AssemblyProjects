e1d0830e92dd813bc17432ad7ff633568b578f4e
Afonso deSousa <af.nunosousa@gmail.com>
2024-03-22 01:06:54 +0000

    made file_finder.sh
    opens dir, for each file creates a unique seed executes .c file with file, a dedicated output file and the unique seed as arguments

--- projeto_1/file_finder.sh
+++ projeto_1/file_finder.sh
@@ -1,1 +1,19 @@
 #!/usr/bin/bash
+
+
+dir=$1
+seed=$2
+
+#opens directory in input;
+cd $dir
+
+#for each file in the directory creates a new seed, concatenating the filename to the seed (seed_file)
+#creates a dedicated output file, file.encripted
+#executes the encription .c file 
+for file in $dir/*
+do
+  seed_file = $seed${file##*/}
+  gcc -o $encript.c $file ${file##*/}.encripted $seed_file
+
+done
+

676e8ee35c546964c4ca48d84c57fd366b3d3ce3
Bernardo Martins <vreabernardo@gmail.com>
2024-03-25 01:49:28 +0000

    rewrite

--- projeto_1/file_finder.sh
+++ projeto_1/file_finder.sh
@@ -5,12 +5,13 @@ if [ "$#" -ne 2 ]; then
     echo "Usage: $0 <directory> <seed>"
     exit 1
 fi
+
 # Assign the input arguments to variables
-directory=$1
+dir=$1
 seed=$2
 
 # Find all files in the input directory and its subdirectories
-files=$(find "$directory" -type f)
+files=$(find "$dir" -type f)
 
 # Iterate over each file found
 for file in $files; do
@@ -18,12 +19,5 @@ for file in $files; do
     if [[ "$file" == *.encrypted ]]; then
         continue  
     fi
-    
-    # Call the C program with the specified arguments
-    original_name=$(basename "$file")
-    dir_name=$(dirname "$file")
-    output_name="${dir_name}/${original_name}.encrypted"
-    new_seed="${seed}${original_name}"
-    
-    ./otp "$file" "$output_name" "$new_seed"
+    ./otp "$file" "${file}.encrypted" "$seed"
 done

9dbf41aeddc7e37733997059a070d9da1d62fcc5
Bernardo Martins <vreabernardo@gmail.com>
2024-03-25 01:48:40 +0000

    codebase rewrite

--- projeto_1/otp.c
+++ projeto_1/otp.c
@@ -1,122 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define ERROR_MESSAGE "Encryption of %s failed.\n"
 #define SUCCESS_MESSAGE "Encryption of %s succeeded.\n"
 
-void encrypt_file(const char *input_file, const char *output_file, const char *key);
-void generate_key(char *key, int length, int seed);
-long get_file_length(const char *input_file);
-void dencrypt_file(const char *input_file, const char *key);
-int generateSeed(char *baseSeed);
-
 int main(int argc, char *argv[])
 {
-    if (argc != 4)
+    FILE *source_file = fopen(argv[1], "r");
+    FILE *destination_file = fopen(argv[2], "w");
+
+    srand(atoi(argv[3]));
+
+    int is_empty = 1;
+
+    char character;
+    while ((character = fgetc(source_file)) != EOF)
     {
-        fprintf(stderr, "Usage: %s <input_file> <output_file> <seed>\n", argv[0]);
-        return 0;
+        is_empty = 0;
+        int random = rand();
+        fputc(character ^ random, destination_file);
+        // printf("%c", character ^ random ^ random); // Test
     }
 
-    int seed = generateSeed(argv[3]);
-
-    long length = get_file_length(argv[1]);
-
-    if (length == 0)
-    {
-        fprintf(stderr, ERROR_MESSAGE, argv[1]);
-        // printf("File is empty\n");
-        return 0;
-    }
-
-    char key[length + 1];
-    generate_key(key, length, seed);
-
-    encrypt_file(argv[1], argv[2], key);
-    // dencrypt_file(argv[2], key); // check if the file is encrypted correctly
-    return 0;
-}
-
-void encrypt_file(const char *input_file, const char *output_file, const char *key)
-{
-    FILE *source_file = fopen(input_file, "r");
-    FILE *destination_file = fopen(output_file, "w");
-
-    int character;
-    int key_char;
-    int position = 0;
-
-    while ((character = fgetc(source_file)) != EOF && key[position] != '\0')
-    {
-        key_char = key[position];
-        fputc(character ^ key_char, destination_file);
-        position++;
-    }
-
-    if (character == EOF && key[position] == '\0')
-    {
-        fprintf(stderr, SUCCESS_MESSAGE, input_file);
-    }
+    if (is_empty)
+        printf(ERROR_MESSAGE, argv[1]);
     else
-    {
-        fprintf(stderr, ERROR_MESSAGE, input_file);
-        // printf("Key is not the same size as the File\n");
-    }
+        printf(SUCCESS_MESSAGE, argv[1]);
 
-    fclose(source_file);
-    fclose(destination_file);
-}
-void generate_key(char *key, int length, int seed)
-{
-    srand(seed);
-    for (int i = 0; i < length; i++)
-    {
-        key[i] = rand();
-    }
-    key[length] = '\0';
-}
-
-int generateSeed(char *baseSeed)
-{
-    int seed = 0;
-
-    // baseSeed = seed number + filename -> aba=aab -> fix: [(idx+1)*a for a in argv[3]]
-    for (int i = 0; baseSeed[i] != '\0'; i++)
-    {
-        seed += (baseSeed[i] * (i + 1));
-    }
-    // printf("Seed: %d\n", seed);
-    return seed;
-}
-
-long get_file_length(const char *input_file)
-{
-    FILE *source_file = fopen(input_file, "r");
-    fseek(source_file, 0, SEEK_END);  // go to end of file
-    long length = ftell(source_file); // get current position on the file
-    fclose(source_file);
-
-    // printf("Length of the file: %ld bytes\n", length);
-    return length;
-}
-
-void dencrypt_file(const char *input_file, const char *key)
-{
-    printf("File decrypted\nData:\n");
-
-    FILE *source_file = fopen(input_file, "r");
-
-    int character;
-    int key_char;
-    int position = 0;
-
-    while ((character = fgetc(source_file)) != EOF && key[position] != '\0')
-    {
-        key_char = key[position];
-        printf("%c", character ^ key_char);
-        position++;
-    }
-    printf("\n");
-    fclose(source_file);
+    return 0;
 }

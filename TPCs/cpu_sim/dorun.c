417710699b33cf51f5f1370c896440f15e9a8582
Bernardo Martins <vreabernardo@gmail.com>
2024-04-13 16:21:08 +0100

    Update dorun.c

--- TPCs/cpu_sim/dorun.c
+++ TPCs/cpu_sim/dorun.c
@@ -1,122 +1,81 @@
-/*
-CPU-Mem architecture
-AC 2023/24  LEI FCT/UNL
-
-256 8-bit data words
-256 16-bit data words
-8 bit addresses (max memory 2^8)
-1 acumulator
-flags: zero, carry, overflow
-
-16 bit instruction
-    4 bits 15-12 opcode
-    4 bits 11-8 specification of ALU operaation ALU 00 ADD 01 SUB
-    8 bits signed value or address
-*/
 #include <stdio.h>
 
 extern unsigned char Mem[];
 extern unsigned short int Code[];
-extern char ac;
+extern char ac;  // 8-bit accumulator
 extern unsigned char flagZero;
 extern unsigned char flagCarry;
 extern unsigned char flagOverflow;
 
-void dorun()
-{
-    unsigned short int pc; // program counter or intruction pointer
-    unsigned short int ir; // instruction register
+void updateFlags(char result) {
+    flagZero = (result == 0);
+    flagCarry = (result < 0) || (result > 255);
+    flagOverflow = ((result < -128) || (result > 127));
+}
+
+void dorun() {
+    unsigned short int pc;  // program counter or instruction pointer
+    unsigned short int ir;  // instruction register
 
     unsigned char opcode;
     unsigned char opALU;
     short addressOrValue;
 
     pc = 0;
-    while (1)
-    {
-        ir = Code[pc];                      // FETCH
-        opcode = (unsigned char)(ir >> 12); // DECODE
-        opALU = (unsigned char)((ir & 0x0f00) >> 8);
-        addressOrValue = ir & 0x00ff;
+    while (1) {
+        ir = Code[pc++];                     // FETCH the instruction at the current PC and increment the PC
+        opcode = (unsigned char)(ir >> 12);  // DECODE opcode
+        opALU = (unsigned char)((ir & 0x0F00) >> 8);  // ALU operation
+        addressOrValue = (char)(ir & 0x00FF);  // sign-extend the lower 8 bits
 
-        printf("PC=%d IR=%04X opcode=%02X opALU=%02X addressOrValue=%02X\n", pc, ir, opcode, opALU, addressOrValue);
+        switch (opcode) {  // EXECUTE based on opcode
+            case 0x0F:  // HALT
+                printf("HALT instruction executed\n");
+                return;
 
-        switch (opcode)
-        {          // EXECUTE
-        case 0x0F: /* HALT */
-            printf("HALT instruction executed\n");
-            return;
+            case 0x00:  // NOOP
+                break;
 
-        case 0x00: /* NOOP */
-            break;
+            case 0x01:  // ADDI / SUBI
+                if (opALU == 0x00) {  // ADDI
+                    ac += addressOrValue;
+                    updateFlags(ac);
+                } else if (opALU == 0x01) {  // SUBI
+                    ac -= addressOrValue;
+                    updateFlags(ac);
+                }
+                break;
 
-        case 0x01: /* ADDI / SUBI */
-            // Accumulator <- Accumulator + vvvvvvvv
-            // Accumulator <- Accumulator - vvvvvvvv
-            opALU == 0x00 ? addi() : subi();
-            break;
+            case 0x02:  // ADD, SUB from memory
+                if (opALU == 0x00) {  // ADD
+                    ac += Mem[addressOrValue];
+                    updateFlags(ac);
+                } else if (opALU == 0x01) {  // SUB
+                    ac -= Mem[addressOrValue];
+                    updateFlags(ac);
+                }
+                break;
 
-        case 0x02: /* ADD, SUB */
-            // Accumulator <- Accumulator + MemoryData[eeeeeeee]
-            // Accumulator <- Accumulator - MemoryData[eeeeeeee]
-            opALU == 0x00 ? add() : sub();
-            break;
+            case 0x03:  // CLAC
+                ac = 0;
+                flagZero = 1;
+                flagCarry = 0;
+                flagOverflow = 0;
+                break;
 
-        case 0x03: /* CLAC */
-            // Accumulator <- 0; set the flag ZERO to 1
-            ac = 0;
-            flagZero = 1;
-            break;
+            case 0x04:  // STORE
+                Mem[addressOrValue] = ac;
+                break;
 
-        case 0x04: /* STORE */
-            // MemoryData[eeeeeeee] = Accumulator
-            Mem[addressOrValue] = ac;
-            break;
+            case 0x05:  // BEQZ
+                if (ac == 0) {
+                    pc = addressOrValue;
+                }
+                break;
 
-        case 0x05: /* BEQZ */
-            // if (Acumuldor == 0) PC <- eeeeeeee
-            if (ac == 0)
-                pc = addressOrValue;
-            break;
-
-        default:
-            printf("Invalid instruction!\n");
-            return;
+            default:
+                printf("Invalid instruction!\n");
+                return;
         }
-        pc++; // NEXT
     }
 }
-void addi()
-{
-    // Accumulator <- Accumulator + vvvvvvvv
-    ac += (char) addressOrValue;
-    
-    flagOverflow = (ac > 0 && addressOrValue > 0 && ac + addressOrValue < 0) 
-    ||(ac < 0 && addressOrValue < 0 && ac + addressOrValue > 0);
-
-    flagZero = ac == 0;
-
-    flagCarry = ac < 0;
-
-}
-void subi()
-{
-    ac += (char) addressOrValue;
-    printf("SUBI\n"); 
-
-
-}
-void add()
-{
-    // Accumulator <- Accumulator + MemoryData[eeeeeeee]
-    ac += Mem[addressOrValue];
-}
-void sub()
-{
-    // Accumulator <- Accumulator - MemoryData[eeeeeeee]
-    ac -= Mem[addressOrValue];
-}
-
-// unsigned carry indicates overflow
-
-// signed overflow ++ = - and -- = +

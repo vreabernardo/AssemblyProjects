f6b621367e53a464f1378ed3ec7a2031d43ffd15
Bernardo Martins <vreabernardo@gmail.com>
2024-04-14 22:14:55 +0100

    same shit but prettier

--- TPCs/cpu_sim/dorun.c
+++ TPCs/cpu_sim/dorun.c
@@ -6,42 +6,24 @@ extern char ac;
 extern unsigned char flagZero;
 extern unsigned char flagCarry;
 extern unsigned char flagOverflow;
-char old_ac;
 
 #define MAX +127
 #define MIN -128
-#define MSB_MASK 0x80
 
-void updateFlagsADD(char result, char a, char b)
+void updateFlags(char result, char a, char b, char isAddition)
 {
-    flagZero = result == 0x00;
+    flagZero = (result == 0x00);
 
     flagCarry = ((unsigned int)a + (unsigned int)b) > 0xFF;
 
-    // when the inputs have opposite signs, and the output has a different sign from the first operand.
-    // * Doest work because of Undefined Behaviour in C, we need to catch the overflow before doing the operation *
-    /* flagOverflow = ((!(a & MSB_MASK) & !((char)b & MSB_MASK) & (result & MSB_MASK)) || // +a + b = -c
-                    ((a & MSB_MASK) & ((char)b & MSB_MASK) & !(result & MSB_MASK)))       // -a - b = +c
-                   >> 7;    // shift from msb to lsb to get only 0 or 1.
-    */
-
-    flagOverflow = (b > 0 && a > MAX - b) || (b < 0 && a < MIN - b);
-}
-
-void updateFlagsSUB(char result, char a, char b)
-{
-    flagZero = result == 0x00;
-
-    flagCarry = ((unsigned int)a + (unsigned int)b) > 0xFF;
-
-    // when the inputs have opposite signs, and the output has a different sign from the first operand.
-    // * Doest work because of Undefined Behaviour in C, we need to catch the overflow before doing the operation *
-    /* flagOverflow = ((!(a & MSB_MASK) & ((char)b & MSB_MASK) & (result & MSB_MASK)) || // +a - b = -c
-                    ((a & MSB_MASK) & !(char)b & MSB_MASK & !(result & MSB_MASK)))       // -a + b = +c
-                   >> 7;    // shift from msb to lsb to get only 0 or 1.
-    */
-
-    flagOverflow = (b > 0 && a < MIN + b) || (b < 0 && a > MAX + b);
+    if (isAddition)
+    {
+        flagOverflow = (b > 0 && a > MAX - b) || (b < 0 && a < MIN - b);
+    }
+    else
+    {
+        flagOverflow = (b < 0 && a > MAX + b) || (b > 0 && a < MIN + b);
+    }
 }
 
 void dorun()
@@ -56,10 +38,10 @@ void dorun()
     pc = 0;
     while (1)
     {
-        ir = Code[pc++];                             // FETCH the instruction at the current PC and increment the PC
-        opcode = (unsigned char)(ir >> 12);          // DECODE opcode
-        opALU = (unsigned char)((ir & 0x0F00) >> 8); // ALU operation
-        addressOrValue = (char)(ir & 0x00FF);        // sign-extend the lower 8 bits
+        ir = Code[pc++];
+        opcode = (unsigned char)(ir >> 12);
+        opALU = (unsigned char)((ir & 0x0F00) >> 8);
+        addressOrValue = (char)(ir & 0x00FF);
 
         /*
         printf("PC: 0x%02x, IR: 0x%04x, opcode: 0x%02x, opALU: 0x%02x, addressOrValue: 0x%02x\n", pc, ir, opcode, opALU, addressOrValue);
@@ -67,7 +49,7 @@ void dorun()
         */
 
         switch (opcode)
-        {          // EXECUTE based on opcode
+        {
         case 0x0F: // HALT
             printf("HALT instruction executed\n");
             return;
@@ -75,39 +57,43 @@ void dorun()
         case 0x00: // NOOP
             break;
 
-        case 0x01: // ADDI / SUBI
-            old_ac = ac;
+        case 0x01: // ADDI, SUBI
+        {
+            char oldAc = ac;
             if (opALU == 0x00)
             { // ADDI
                 ac += addressOrValue;
-
-                updateFlagsADD(ac, old_ac, addressOrValue);
+                updateFlags(ac, oldAc, addressOrValue, 1);
             }
             else if (opALU == 0x01)
             { // SUBI
                 ac -= addressOrValue;
-
-                updateFlagsSUB(ac, old_ac, addressOrValue);
+                updateFlags(ac, oldAc, addressOrValue, 0);
             }
             break;
+        }
 
-        case 0x02: // ADD, SUB from memory
-            old_ac = ac;
+        case 0x02: // ADD, SUB
+        {
+            char oldAc = ac;
             if (opALU == 0x00)
             { // ADD
                 ac += (char)Mem[addressOrValue];
-                updateFlagsADD(ac, old_ac, Mem[addressOrValue]);
+                updateFlags(ac, oldAc, Mem[addressOrValue], 1);
             }
             else if (opALU == 0x01)
             { // SUB
                 ac -= (char)Mem[addressOrValue];
-                updateFlagsSUB(ac, old_ac, Mem[addressOrValue]);
+                updateFlags(ac, oldAc, Mem[addressOrValue], 0);
             }
             break;
+        }
 
-        case 0x03: // CLAC
+        case 0x03: // CLAC "clear acumulator"
             ac = 0;
             flagZero = 1;
+            flagCarry = 0;
+            flagOverflow = 0;
             break;
 
         case 0x04: // STORE

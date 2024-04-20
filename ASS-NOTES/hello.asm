40fccc0dd88cbc122fa40af24f1c7755256e77cb
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-04-20 18:32:02 +0100

    auto commit from alias

---
+++ ASS-NOTES/hello.asm
@@ -0,0 +1,19 @@
+global _start
+
+section .text:
+
+_start:
+	mov eax, 0x4		; use the write syscall
+	mov ebx, 1		; use stdout as the fd
+	mov ecx, message
+	mov edx, message_lenght
+	int 0x80		; invoke syscall
+	
+	;			exit
+	mov eax, 0x1
+	mov ebx, 0
+	int 0x80
+
+section .data:
+	message: db "Hello World!", 0xA
+	message_lenght equ $-message

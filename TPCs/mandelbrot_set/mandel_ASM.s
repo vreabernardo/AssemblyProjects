e31d8b2374b2ab64c9deeebb95f5507e334934b4
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-05-07 14:56:01 +0100

    init tpc3

---
+++ TPCs/mandelbrot_set/mandel_ASM.s
@@ -0,0 +1,21 @@
+.globl updateImage  #  void updateImage( buffer, x,     y,  val, base)
+.globl computePoint #  unsigned char computePoint( double x, double y)
+
+.section .note.GNU-stack,"",@progbits
+.text
+
+
+updateImage:
+
+    retq
+
+
+
+
+computePoint:        
+
+    retq
+
+
+
+                                    

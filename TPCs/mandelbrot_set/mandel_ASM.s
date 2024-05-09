a81c76d31e0796d8249957c48a64c6624a9ce035
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-05-09 12:52:12 +0100

    argument support

--- TPCs/mandelbrot_set/mandel_ASM.s
+++ TPCs/mandelbrot_set/mandel_ASM.s
@@ -3,20 +3,20 @@
 .section .note.GNU-stack,"",@progbits
 .text
 
-# rdi  255
+# rcx  255
 # rcx  i
 # xmm0 zi
 # xmm1 zr
 
-computePoint:
-    movq $255, %rdi             # rdi = 255 max iterations
+computePoint:                   # rdi x_value rsi y_value
+    movq $255, %rcx             # rcx = 255 max iterations
     movq $0, %rax               # rcx = 0 current iteration
     movsd zero(%rip), %xmm0     # xmm0 = 0.0
     movsd zero(%rip), %xmm1     # xmm1 = 0.0    
     
-    cycle:
-    cmpq %rax, %rdi
-    je end                      # if rax == rdi, jump to end aka i == 255
+    computePoint_cycle:
+    cmpq %rax, %rcx
+    je computePoint_end         # if rax == rcx, jump to end aka i == 255
 
 
     movsd %xmm0, %xmm2          # xmm2 = zi
@@ -25,13 +25,13 @@ computePoint:
     mulsd %xmm3, %xmm3          # xmm3 = zr^2
     addsd %xmm2, %xmm3          # xmm3 = zr^2 + zi^2
     ucomisd four(%rip), %xmm3   # compare xmm3 to 4.0
-    jae end                     # if xmm3 >= 4.0, jump to end
+    jae computePoint_end        # if xmm3 >= 4.0, jump to end
     
 
     incq %rax
-    jmp cycle
+    jmp computePoint_cycle
 
-end:
+computePoint_end:
     retq
 
 zero:

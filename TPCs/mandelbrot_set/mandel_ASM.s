904ca441d501d1ce45e0a244f17e12876a50dc2d
vreabernardo@ubuntu <vreabernardo@ubuntu>
2024-05-09 12:40:43 +0100

    Refactor mandel_ASM.s for improved readability

--- TPCs/mandelbrot_set/mandel_ASM.s
+++ TPCs/mandelbrot_set/mandel_ASM.s
@@ -9,23 +9,23 @@
 # xmm1 zr
 
 computePoint:
-    movq $255, %rdi # rdi = 255 max iterations
-    movq $0, %rax   # rcx = 0 current iteration
-    movsd zero(%rip), %xmm0 # xmm0 = 0.0
-    movsd zero(%rip), %xmm1 # xmm1 = 0.0    
+    movq $255, %rdi             # rdi = 255 max iterations
+    movq $0, %rax               # rcx = 0 current iteration
+    movsd zero(%rip), %xmm0     # xmm0 = 0.0
+    movsd zero(%rip), %xmm1     # xmm1 = 0.0    
     
     cycle:
     cmpq %rax, %rdi
-    je end     # if rax == rdi, jump to end aka i == 255
+    je end                      # if rax == rdi, jump to end aka i == 255
 
 
-    movsd %xmm0, %xmm2 # xmm2 = zi
-    movsd %xmm1, %xmm3 # xmm3 = zr
-    mulsd %xmm2, %xmm2 # xmm2 = zi^2
-    mulsd %xmm3, %xmm3 # xmm3 = zr^2
-    addsd %xmm2, %xmm3 # xmm3 = zr^2 + zi^2
-    ucomisd four(%rip), %xmm3 # compare xmm3 to 4.0
-    jae end # if xmm3 >= 4.0, jump to end
+    movsd %xmm0, %xmm2          # xmm2 = zi
+    movsd %xmm1, %xmm3          # xmm3 = zr
+    mulsd %xmm2, %xmm2          # xmm2 = zi^2
+    mulsd %xmm3, %xmm3          # xmm3 = zr^2
+    addsd %xmm2, %xmm3          # xmm3 = zr^2 + zi^2
+    ucomisd four(%rip), %xmm3   # compare xmm3 to 4.0
+    jae end                     # if xmm3 >= 4.0, jump to end
     
 
     incq %rax
